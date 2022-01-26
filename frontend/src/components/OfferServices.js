bf9927d8fc057668fc5b951dc0170ffe0a63e503
alvarobernal2412 <alvarobc2412@gmail.com>
2022-01-26 13:28:42 +0100

    Add frontend offer contract listing

--- frontend/src/components/OfferServices.js
+++ frontend/src/components/OfferServices.js
@@ -6,6 +6,43 @@ const OfferServices = () => {
     const [web3Provider, setProvider] = useState([]);
     const [account, setAccount] = useState(["not connected"]);
     const [connectedTest, setConnected] = useState(["Connect"]);
+    const [contract,setContract]= useState([]);
+    const [offers,setOffers]= useState([]);
+
+    useEffect (async () => {
+        const res2 = require("../../node_modules/@truffle/contract/dist/truffle-contract")
+        const res = await fetch("../../../backend/build/OfferServices.json")
+        const offerContractJson = await res.json()
+        const offerContract = TruffleContract(offerContractJson)
+        offerContract.setProvider(web3Provider)
+        await setContract(offerContract.deployed())
+        const offerCounter = await contract.offerCounter()
+        const offerCounterNum = offerCounter.toNumber()
+        
+        let html =''
+        for (let i = 1; i <= offerCounterNum; i++) {
+            
+            const offer = contract.offers(i)
+            const title = offer[1]
+            const description = offer[2]
+            const owner = offer[3]
+            const createdAt = offer[4]
+            let offerElement = `
+            <div class="card bg-dark rounded-0 mb-2">
+                <div class="card-header d-flex justify-content-between align-items-center">
+                    <span>${title} - ${owner}</span>
+                </div>
+                <div class="card-body">
+                    <span>${description}</span>
+                    <p class="text-muted">Created at: ${new Date(createdAt * 1000).toLocaleString()}</p>
+                </div>
+            </div>
+            `
+            html += offerElement
+
+        }
+        setOffers(html)
+    },[])
 
     async function loadEthereum(){
         if (window.ethereum){
@@ -23,18 +60,22 @@ const OfferServices = () => {
         <div>
             <div class="row">
                 <div class="col-md-11">
-                    <h2 class="c">dServices</h2>
-                    <p>Your account is: {account}</p>
+                    <h1 class="c">dServices</h1>
+                    <p class="text-muted">Your account is: {account}</p>
                 </div>
                 <div class="col-md-1 py-3">
-                    <Button disabled={connectedTest == "Connected"} onClick={loadEthereum} label={connectedTest} className="p-button-raised p-button-warning p-button-text" />
+                    <Button disabled={connectedTest == "Connected"} onClick={loadEthereum} label={connectedTest} className="p-button-raised p-button-warning" />
                 </div>
             </div>
-            <div class="card">
+            <div class="card bg-dark">
                 <div class="card-header">
                     <h4>Offer Services</h4>
                 </div>
+                <div class="card-body">
+                    {offers}
+                </div>
             </div>
+
         </div>
     )
 }
